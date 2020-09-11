fedaf1927e2b6b6461516cb297530e089d2efd62
Fridry <fredwin.ed@gmai.com>
2020-09-11 17:28:18 -0300

    Limpeza inicial e criação do template

---
+++ src/components/Template.js
@@ -0,0 +1,44 @@
+import React, { Fragment, useState } from "react";
+
+import "./assets/css/styles.css";
+import Footer from "./Footer";
+
+import Menu from "./Menu";
+import Sidebar from "./Sidebar";
+
+const Template = ({ children, title }) => {
+  const [hide, setHide] = useState(false);
+
+  const classSidebar = hide
+    ? "sb-nav-fixed sb-sidenav-toggled"
+    : "sb-nav-fixed";
+
+  const toggleSidebar = () => setHide(!hide);
+
+  return (
+    <Fragment>
+      <div className={classSidebar}>
+        <Menu toggleSidebar={toggleSidebar} />
+
+        <div id="layoutSidenav">
+          <div id="layoutSidenav_nav">
+            <Sidebar />
+          </div>
+
+          <div id="layoutSidenav_content">
+            <main>
+              <div className="container-fluid">
+                <h1 className="mt-4">{title}</h1>
+                {children}
+              </div>
+            </main>
+
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Template;
